9dd419ef04218dc354509725b1838ead7cfbd0ca
gagan2017-soc <65751237+gagan2017-soc@users.noreply.github.com>
2020-07-01 18:48:27 +0530

    Update server.js
    ne cnage

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/gaganclient'));
-app.use("/images", express.static(path.join(__dirname, "images")));
-
+//app.use("/images", express.static(path.join(__dirname, "images")));
+console.log('Node.js web server at port 5000 is running..')
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 
-}
+};
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+console.log('Node.js web server at port 5000 is running..'+ process.env.PORT);
