9ba8e06cdae7a62939a64874f7d45f06898d7461
gagan2017-soc <65751237+gagan2017-soc@users.noreply.github.com>
2020-07-01 18:10:56 +0530

    Update server.js
    udated pag

--- server.js
+++ server.js
@@ -8,10 +8,9 @@ const app = express();
 app.use(express.static(__dirname + '/dist/gaganclient'));
 app.use("/images", express.static(path.join(__dirname, "images")));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/gaganclient/index.html'));
-});
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
