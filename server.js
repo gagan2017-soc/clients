65c9f38b76c7ac570365c7699db3b1451611fba1
gagan2017-soc <65751237+gagan2017-soc@users.noreply.github.com>
2020-07-01 18:32:54 +0530

    Update server.js
    complw

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.use("/images", express.static(path.join(__dirname, "images")));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 
-
+}
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
